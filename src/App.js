80c13bcd296202c9e48835217c199d824bd321dd
Ujjwal Giri <ucshgiri11@gmail.com>
2022-07-04 08:37:22 +0530

    new1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { Component } from "react"; 
 import './App.css';
+import M from "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
+import DogBreeds from "./components/DogsBreeds";
 
-function App() {
-  return (
+
+
+
+class App extends Component{
+  // add(a,b){return a+b;}
+
+  state = {
+    visible: true
+  }
+
+  // handleVote = () => {
+  //   <Vote/>
+  // }
+
+  render() {  
+    /*var add = (a,b) => a+b;*/
+    // let slider = this.state.visible ? (<Imageloder/>) : (<div>This is hidden</div>);
+
+    // if(!this.state.visible){
+    //   slider = <div>display nothing</div>;
+    // }
+
+    // const buttonText = this.state.visible ? 'hide' : 'show';
+    
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  class="col s12">
+      <ul style={{backgroundColor: "black"}} class="tabs">
+        {/* <li class="tab col s3"><a class="active" href="#test1">VOTE</a></li> */}
+        <li class="tab col s3"><a class="active" href="#test2">BREEDS</a></li>
+        {/* <li class="tab col s3"><a class="active" href="#test3">IMAGES/SEARCH</a></li>
+        <li class="tab col s3"><a class="active" href="#test4">FAVOURITES</a></li>
+        <li class="tab col s3"><a class="active" href="#test4">UPLOAD</a></li> */}
+      </ul>
     </div>
-  );
+
+    
+    <div id="test1" class="col s12">{<DogBreeds/>}</div>
+  
+      {/* <Header title={"Hello Bro"} num={10}
+      myArr = {[2,4,6]}
+      myFunc = {this.add}
+      myObj = {{
+        a: 90,
+        b: 80
+      }}/>
+
+      <Body2/>
+      <Body3/>
+      <Body text={"are sahi hai bhai ye functional component"} text2={"dekh ye 2nd text"} myFunc={this.add}/>
+      <Counter initialCount={0}/>
+      <Counter initialCount={10}/> */}
+
+      {/* {slider}
+
+      <button onClick={()=>{
+        this.setState({visible: !this.state.visible});
+      }}>
+        {buttonText}
+      </button> */}
+
+      {/* <MyForm/> */}
+      
+    </div>
+  
+);
+  }
 }
+
 
 export default App;
---
+++ src/components/DogsBreeds.js
@@ -0,0 +1,25 @@
+import Select from "react-select";
+import { useState, useEffect } from "react";
+
+const DogBreeds = () => {
+    let breedNames = []
+    const [options, setOptions] = useState()
+    const url = "https://api.thecatapi.com/v1/breeds?api_key=a75dc5be-ed7a-4ea0-9c74-10ed2648c2bf";
+    fetch(url, {
+        // body: {
+        //     api_key:'a75dc5be-ed7a-4ea0-9c74-10ed2648c2bf'
+        // }
+    }).then(resp => resp.json())
+    .then(data => {
+        breedNames = data.filter(i => breedNames.push(i.name))
+        setOptions(breedNames.map(b => ({value: b.name, label: b.name})))
+    })
+
+    return (
+    <Select
+          options={options}
+        />
+    )
+}
+
+export default DogBreeds
