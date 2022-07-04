84b5507f6c1d64adb863c8eacbf9356b7d4b90ad
Ujjwal Giri <ucshgiri11@gmail.com>
2022-07-04 10:15:39 +0530

    new2

--- src/components/DogsBreeds.js
+++ src/components/DogsBreeds.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 const DogBreeds = () => {
     let breedNames = []
     const [options, setOptions] = useState()
-    const url = "https://api.thecatapi.com/v1/breeds?api_key=a75dc5be-ed7a-4ea0-9c74-10ed2648c2bf";
+    const url = "https://api.thedogapi.com/v1/breeds?api_key=a75dc5be-ed7a-4ea0-9c74-10ed2648c2bf";
     fetch(url, {
         // body: {
         //     api_key:'a75dc5be-ed7a-4ea0-9c74-10ed2648c2bf'
@@ -12,7 +12,7 @@ const DogBreeds = () => {
     }).then(resp => resp.json())
     .then(data => {
         breedNames = data.filter(i => breedNames.push(i.name))
-        setOptions(breedNames.map(b => ({value: b.name, label: b.name})))
+        setOptions(breedNames.map(b => ({value: b.name, label: b.name})),)
     })
 
     return (
